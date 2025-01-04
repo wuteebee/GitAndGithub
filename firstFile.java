868008f6185d5d2b0ad6648ecb58b054add1667c
wuteebee <185331818+wuteebee@users.noreply.github.com>
2025-01-04 15:07:42 +0700

    modified:   firstFile.java 	new file:   index.html

--- firstFile.java
+++ firstFile.java
@@ -2,4 +2,8 @@ public class firstFile {
     public void printFirst() {
         System.out.println("This is the first file.");
     }
+
+    public void printNew() {
+        System.out.println("This is a new addition.");
+    }
 }
